d3669418474871cfbcfa2b58efc0baf119c6b1ef
Chelsea Carr <674203+SheikGeek@users.noreply.github.com>
2022-12-01 22:48:51 -0600

    Finished part 2

--- advent_2022/Advent2022/Days/DayOne/DayOnePartTwo.swift
+++ advent_2022/Advent2022/Days/DayOne/DayOnePartTwo.swift
@@ -10,17 +10,13 @@ import Foundation
 import UIKit
 
 /*
---- Part Two ---
-During the second Go / No Go poll, the Elf in charge of the Rocket Equation Double-Checker stops the launch sequence. Apparently, you forgot to include additional fuel for the fuel you just added.
+ By the time you calculate the answer to the Elves' question, they've already realized that the Elf carrying the most Calories of food might eventually run out of snacks.
 
-Fuel itself requires fuel just like a module - take its mass, divide by three, round down, and subtract 2. However, that fuel also requires fuel, and that fuel requires fuel, and so on. Any mass that would require negative fuel should instead be treated as if it requires zero fuel; the remaining mass, if any, is instead handled by wishing really hard, which has no mass and is outside the scope of this calculation.
+ To avoid this unacceptable situation, the Elves would instead like to know the total Calories carried by the top three Elves carrying the most Calories. That way, even if one of those Elves runs out of snacks, they still have two backups.
 
-So, for each module mass, calculate its fuel and add it to the total. Then, treat the fuel amount you just calculated as the input mass and repeat the process, continuing until a fuel requirement is zero or negative. For example:
+ In the example above, the top three Elves are the fourth Elf (with 24000 Calories), then the third Elf (with 11000 Calories), then the fifth Elf (with 10000 Calories). The sum of the Calories carried by these three elves is 45000.
 
-A module of mass 14 requires 2 fuel. This fuel requires no further fuel (2 divided by 3 and rounded down is 0, which would call for a negative fuel), so the total fuel required is still just 2.
-At first, a module of mass 1969 requires 654 fuel. Then, this fuel requires 216 more fuel (654 / 3 - 2). 216 then requires 70 more fuel, which requires 21 fuel, which requires 5 fuel, which requires no further fuel. So, the total fuel required for a module of mass 1969 is 654 + 216 + 70 + 21 + 5 = 966.
-The fuel required by a module of mass 100756 and its fuel is: 33583 + 11192 + 3728 + 1240 + 411 + 135 + 43 + 12 + 2 = 50346.
-What is the sum of the fuel requirements for all of the modules on your spacecraft when also taking into account the mass of the added fuel? (Calculate the fuel requirements for each module separately, then add them all up at the end.)
+ Find the top three Elves carrying the most Calories. How many Calories are those Elves carrying in total?
 */
 
 class DayOnePartTwo: DayOne {
@@ -30,12 +26,12 @@ class DayOnePartTwo: DayOne {
     }
     
     override func description() -> String {
-        return "What is the sum of the fuel requirements for all of the modules on your spacecraft when also taking into account the mass of the added fuel?"
+        return "Find the top three Elves carrying the most Calories. How many Calories are those Elves carrying in total?"
     }
    
     override func answer() -> DayAnswer {
         let solution = solveProblem()
-        let alert = UIAlertController(title: "Answer for \(partTitle())", message: "The total fuel needed for all fuel is \(solution)", preferredStyle: .alert)
+        let alert = UIAlertController(title: "Answer for \(partTitle())", message: "The total number of calories carried by the top 3 elves is \(solution) Calories", preferredStyle: .alert)
         
         alert.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
         
@@ -46,25 +42,45 @@ class DayOnePartTwo: DayOne {
 private extension DayOnePartTwo {
     
     func solveProblem() -> String {
-//        let modules = parseAndSplitData()
-//        var total = 0
-//
-//        for module in modules {
-//
-//            let initialFuelAmount = calculateFuelFromString(for: module)
-//            let totalFuelAmount = Int(moduleTotal(from: initialFuelAmount))
-//            total += totalFuelAmount
-//        }
-//
-//        return "\(total)"
-        return ""
-    }
+        let lines = parseAndSplitData()
+        
+        var total: Int = 0
+        var calories = [Int]()
 
-    private func moduleTotal(from currentModule: Double) -> Double {
-//        let fuelAmount = calculateFuel(for: currentModule)
-//
-//        guard fuelAmount > 0 else { return currentModule }
-//        return moduleTotal(from: fuelAmount) + currentModule
-        return 0
+        lines.forEach {
+            guard !$0.isEmpty else {
+                if let index = calories.firstIndex(where: { $0 < total }) {
+                    calories.insert(total, at: index)
+                    calories.removeLast()
+                } else {
+                    calories.append(total)
+                }
+                
+                total = 0
+                
+                return
+            }
+            
+            total += Int($0) ?? 0
+        }
+    
+        var finalTotal: Int = 0
+        var totalAdded = false
+        var numOfCaloriesAdded = 0
+        for calorie in calories {
+            guard numOfCaloriesAdded < 3 else { break }
+            if total > calorie && !totalAdded {
+                totalAdded = true
+                finalTotal += total
+                numOfCaloriesAdded += 1
+            }
+            
+            guard numOfCaloriesAdded < 3 else { break }
+            
+            finalTotal += calorie
+            numOfCaloriesAdded += 1
+        }
+               
+        return ("\(finalTotal)")
     }
 }

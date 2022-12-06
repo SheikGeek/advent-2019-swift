4b9b99893a25327702c013b2c9447a1a149ad182
Chelsea Carr <674203+SheikGeek@users.noreply.github.com>
2022-12-05 23:11:05 -0600

    completed part 2

--- advent_2022/Advent2022/Days/DayFive/DayFivePartTwo.swift
+++ advent_2022/Advent2022/Days/DayFive/DayFivePartTwo.swift
@@ -9,18 +9,48 @@ import Foundation
 import UIKit
 
 /*
- --- Part Two ---
- It seems like there is still quite a bit of duplicate work planned. Instead, the Elves would like to know the number of pairs that overlap at all.
+ As you watch the crane operator expertly rearrange the crates, you notice the process isn't following your prediction.
 
- In the above example, the first two pairs (2-4,6-8 and 2-3,4-5) don't overlap, while the remaining four pairs (5-7,7-9, 2-8,3-7, 6-6,4-6, and 2-6,4-8) do overlap:
+ Some mud was covering the writing on the side of the crane, and you quickly wipe it away. The crane isn't a CrateMover 9000 - it's a CrateMover 9001.
 
- 5-7,7-9 overlaps in a single section, 7.
- 2-8,3-7 overlaps all of the sections 3 through 7.
- 6-6,4-6 overlaps in a single section, 6.
- 2-6,4-8 overlaps in sections 4, 5, and 6.
- So, in this example, the number of overlapping assignment pairs is 4.
+ The CrateMover 9001 is notable for many new and exciting features: air conditioning, leather seats, an extra cup holder, and the ability to pick up and move multiple crates at once.
 
- In how many assignment pairs do the ranges overlap?
+ Again considering the example above, the crates begin in the same configuration:
+
+     [D]
+ [N] [C]
+ [Z] [M] [P]
+  1   2   3
+ Moving a single crate from stack 2 to stack 1 behaves the same as before:
+
+ [D]
+ [N] [C]
+ [Z] [M] [P]
+  1   2   3
+ However, the action of moving three crates from stack 1 to stack 3 means that those three moved crates stay in the same order, resulting in this new configuration:
+
+         [D]
+         [N]
+     [C] [Z]
+     [M] [P]
+  1   2   3
+ Next, as both crates are moved from stack 2 to stack 1, they retain their order as well:
+
+         [D]
+         [N]
+ [C]     [Z]
+ [M]     [P]
+  1   2   3
+ Finally, a single crate is still moved from stack 1 to stack 2, but now it's crate C that gets moved:
+
+         [D]
+         [N]
+         [Z]
+ [M] [C] [P]
+  1   2   3
+ In this example, the CrateMover 9001 has put the crates in a totally different order: MCD.
+
+ Before the rearrangement process finishes, update your simulation so that the Elves know where they should stand to be ready to unload the final supplies. After the rearrangement procedure completes, what crate ends up on top of each stack?
 */
 
 class DayFivePartTwo: DayFive {
@@ -29,13 +59,12 @@ class DayFivePartTwo: DayFive {
     }
     
     override func description() -> String {
-        return " In how many assignment pairs do the ranges overlap?"
+        return "After the rearrangement procedure completes, what crate ends up on top of each stack?"
     }
    
     override func answer() -> DayAnswer {
         let solution = solveProblem()
-        let alert = UIAlertController(title: "Answer for \(partTitle())", message: "The number of assignment pairs that overlap is \(solution)", preferredStyle: .alert)
-
+        let alert = UIAlertController(title: "Answer for \(partTitle())", message: "The crate on top of each stack is \(solution)", preferredStyle: .alert)
         
         alert.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
         
@@ -47,12 +76,35 @@ private extension DayFivePartTwo {
     
     func solveProblem() -> String {
         let (lines, stacks) = parseAndSplitData()
-        var total: String = ""
-        lines.forEach {
-            $0
+        var mutableStacks = stacks
+
+        for line in lines {
+            guard line != "" else { break }
+            let sanitizedArray = line.components(separatedBy: .whitespaces)
+            var moves = [Int]()
+            for element in sanitizedArray {
+                guard let num = Int(element) else { continue }
+                moves.append(num)
+            }
+            
+            var numToRemove = moves.first ?? 0
+            var itemsToMove = [String]()
+            let from = moves[1] - 1
+            let to = (moves.last ?? 0) - 1
+            
+            while numToRemove > 0 {
+                let item = mutableStacks[from].popLast() ?? ""
+                itemsToMove.insert(item, at: 0)
+                numToRemove -= 1
+            }
+            mutableStacks[to].append(contentsOf: itemsToMove)
         }
         
+        var finalString = "\n"
+        for stack in mutableStacks {
+            finalString += (stack.last ?? "")
+        }
         
-        return total
+        return finalString
     }
 }
